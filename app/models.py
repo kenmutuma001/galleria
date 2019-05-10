9dcecaed75d8d81270e095af9a82ad92c42928e7
kennedymutuma <santa@northpole.com>
2019-05-10 04:09:41 +0300

    define the filter image method

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+import datetime as dt
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFit
+
+# Create your models here.
+
+class Album(models.Model):
+    
+    
+class Image(models.Model):
+    
+    
+class Location(models.Model):
+    
+    
+class Category(models.Model):        
